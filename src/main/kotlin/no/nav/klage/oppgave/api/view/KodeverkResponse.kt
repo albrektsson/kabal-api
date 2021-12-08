af5d5339992023ce2df5ea85ff7e4d088e1559f5
Øyvind Norsted Wedøe <oyvind.norsted.wedoe@nav.no>
2021-12-08 14:01:58 +0100

    Uses registreringshjemmel as basis for kodeverk.

--- src/main/kotlin/no/nav/klage/oppgave/api/view/KodeverkResponse.kt
+++ src/main/kotlin/no/nav/klage/oppgave/api/view/KodeverkResponse.kt
@@ -1,9 +1,11 @@
 package no.nav.klage.oppgave.api.view
 
 import no.nav.klage.kodeverk.*
+import no.nav.klage.kodeverk.hjemmel.Registreringshjemmel
+import no.nav.klage.kodeverk.hjemmel.ytelseTilRegistreringshjemler
 
 data class KodeverkResponse(
-    val hjemmel: List<Kode> = Hjemmel.values().asList().toDto(),
+    val hjemmel: List<Kode> = getHjemler(),
     val type: List<Kode> = Type.values().asList().toDto(),
     val tema: List<Kode> = Tema.values().asList().toDto(),
     val ytelse: List<Kode> = Ytelse.values().asList().toDto(),
@@ -23,10 +25,67 @@ data class HjemlerPerYtelse(val ytelseId: String, val hjemler: List<KodeDto>)
 
 fun Kode.toDto() = KodeDto(id, navn, beskrivelse)
 
+fun Registreringshjemmel.toDto() = KodeDto(
+    id = this.id,
+    navn = this.lovKilde.beskrivelse + " - " + this.spesifikasjon,
+    beskrivelse = this.lovKilde.navn + " - " + this.spesifikasjon,
+)
+
 fun List<Kode>.toDto() = map { it.toDto() }
 
-fun hjemlerPerTema(): List<HjemlerPerTema> = hjemlerPerTema.map { HjemlerPerTema(it.tema.id, it.hjemler.toDto()) }
+fun hjemlerPerTema(): List<HjemlerPerTema> =
+    temaToHjemmelList.map { HjemlerPerTema(it.tema.id, it.hjemler.map { hjemmel -> hjemmel.toDto() }) }
 
-fun hjemlerPerYtelse(): List<HjemlerPerYtelse> = hjemlerPerYtelse.map { HjemlerPerYtelse(it.ytelse.id, it.hjemler.toDto()) }
+fun hjemlerPerYtelse(): List<HjemlerPerYtelse> =
+    ytelseToHjemmelList.map { HjemlerPerYtelse(it.ytelse.id, it.hjemler.map { hjemmel -> hjemmel.toDto() }) }
 
 
+fun getHjemler(): List<KodeDto> =
+    Registreringshjemmel.values().map {
+        KodeDto(
+            id = it.id,
+            navn = it.lovKilde.beskrivelse + " - " + it.spesifikasjon,
+            beskrivelse = it.lovKilde.navn + " - " + it.spesifikasjon,
+        )
+    }
+
+data class TemaToRegistreringshjemler(val tema: Tema, val hjemler: List<Registreringshjemmel>)
+
+val temaToHjemmelList: List<TemaToRegistreringshjemler> = listOf(
+    TemaToRegistreringshjemler(
+        Tema.OMS,
+        ytelseTilRegistreringshjemler[Ytelse.OMS_PLS]
+            ?.plus(ytelseTilRegistreringshjemler[Ytelse.OMS_OLP])
+            ?.plus(ytelseTilRegistreringshjemler[Ytelse.OMS_OMP])
+            ?.plus(ytelseTilRegistreringshjemler[Ytelse.OMS_PSB])?.distinct() as List<Registreringshjemmel>
+    ),
+    TemaToRegistreringshjemler(
+        Tema.SYK,
+        ytelseTilRegistreringshjemler[Ytelse.SYK_SYK]!!
+    )
+)
+
+data class YtelseToRegistreringshjemler(val ytelse: Ytelse, val hjemler: List<Registreringshjemmel>)
+
+val ytelseToHjemmelList: List<YtelseToRegistreringshjemler> = listOf(
+    YtelseToRegistreringshjemler(
+        Ytelse.OMS_PLS,
+        ytelseTilRegistreringshjemler[Ytelse.OMS_PLS]!!
+    ),
+    YtelseToRegistreringshjemler(
+        Ytelse.OMS_PSB,
+        ytelseTilRegistreringshjemler[Ytelse.OMS_PSB]!!
+    ),
+    YtelseToRegistreringshjemler(
+        Ytelse.OMS_OMP,
+        ytelseTilRegistreringshjemler[Ytelse.OMS_OMP]!!
+    ),
+    YtelseToRegistreringshjemler(
+        Ytelse.OMS_OLP,
+        ytelseTilRegistreringshjemler[Ytelse.OMS_OLP]!!
+    ),
+    YtelseToRegistreringshjemler(
+        Ytelse.SYK_SYK,
+        ytelseTilRegistreringshjemler[Ytelse.SYK_SYK]!!
+    )
+)
